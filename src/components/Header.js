51ac9cbbc14d31444297c638ae89a30f5d1a656b
abhishek <64072630+abhishekpawar1402@users.noreply.github.com>
2021-04-17 11:00:41 +0530

    changed server port address

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import Progress from './Progress';
+import React from "react";
+import Progress from "./Progress";
 
 const Header = () => (
   <div>
     <h1>Multi Step Registration</h1>
+    <a href="http://localhost:3030/login">Login</a>
     <Progress />
   </div>
 );
